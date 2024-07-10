fe025a013b62732daab7d9faa872e15160e49fae
Ever12349 <qianxiao0762@outlook.com>
2024-07-10 18:54:40 +0800

    123

--- kasarisute-user/src/main/java/com/kasarisute/domain/User.java
+++ kasarisute-user/src/main/java/com/kasarisute/domain/User.java
@@ -13,6 +13,7 @@ import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EntityListeners;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
@@ -37,7 +38,7 @@ public class User {
     @Comment("用户ID")
     private String uid;
 
-    @OneToOne(cascade = CascadeType.PERSIST)
+    @OneToOne(cascade = CascadeType.PERSIST,fetch = FetchType.LAZY)
     @JoinColumn(name = "pwId")
     private UserPw userPw;
 
