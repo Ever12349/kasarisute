9dc6e3b0787e8ef0c2d1af2c6b843b035f75156c
Ever12349 <qianxiao0762@outlook.com>
2024-07-19 00:09:44 +0800

    删除多余的文件

--- kasarisute-user/src/main/java/com/kasarisute/repositories/SampleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.kasarisute.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.kasarisute.entitys.SampleTable;
-
-public interface SampleRepository extends JpaRepository<SampleTable,Long> {
-
-}
